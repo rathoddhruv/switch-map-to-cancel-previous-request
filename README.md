e04c73455d6c9d8e8587de07c001c0ea1c9e8bc8
rathoddhruv <dhruvthenorthstar@gmail.com>
2020-09-09 16:18:47 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-rxjs-switchmap-merge-results-multiple-http-reque-yyq7sn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-rxjs-switchmap-merge-results-multiple-http-reque-yyq7sn)
---
+++ src/app/app.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { Subject } from "rxjs/Subject";
+import { NgModel } from "@angular/forms";
+import {
+  concatMap,
+  mergeMap,
+  switchMap,
+  map,
+  take,
+  delay,
+  from
+} from "rxjs/operators";
+import "rxjs/add/observable/from";
+import { HttpClient } from "@angular/common/http";
+import { timer } from "rxjs";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  searchString: Subject<string> = new Subject();
+  searchString$: Observable<string> = this.searchString.asObservable();
+  switch$: Observable<string>;
+  concat$: Observable<string>;
+  merge$: Observable<string>;
+  switch:any;
+
+  updateSearch(value) {
+    this.searchString.next(value);
+    this.buttonClick().subscribe(x => {
+      // debugger
+      console.log(x);
+      this.switch = JSON.stringify(x);
+    });
+  }
+
+  ngOnInit() {
+    // this.switch$ = this.searchString$.pipe(
+    //   switchMap(x =>
+    //     this.http.get("https://jsonplaceholder.typicode.com/users").pipe(
+    //       delay(1000),
+    //       map(y => `outer: ${x}, inner: ${y[0]}`)
+    //     )
+    //   )
+    // );
+
+    // this.buttonClick().subscribe(x => (this.switch$ = x));
+  }
+
+  buttonClick() {
+    // debugger
+    return timer(100,10).pipe(
+    // return this.searchString.pipe(
+      switchMap(y => {
+         return this.http.get("https://jsonplaceholder.typicode.com/users").pipe(map(
+           z => {
+             return z;
+           }
+         )
+         );
+      })
+    );
+  }
+}
